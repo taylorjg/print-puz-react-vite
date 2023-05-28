2ca9a13408f7f36e6e618960a8f056f6dcea53e9
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-05-28 23:42:46 +0100

    A couple of little experiments re styled components and gif assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
+import { StyledDiv } from "./App.styles";
+import myImage from "./assets/10_number4.gif";
 
 export const App = () => {
   const url = "https://news.bbc.co.uk";
 
   return (
     <div>
-      <div>Hello from App</div>
+      <StyledDiv>Hello from App</StyledDiv>
       <Link to={"/page2"} state={{ url }}>
         Page 2
       </Link>
+      <img src={myImage} alt="Number 10" />
     </div>
   );
 };
