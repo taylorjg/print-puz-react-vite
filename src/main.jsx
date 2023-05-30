740b171e3d38b5905f4b1dae3521fc3de59ea876
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-05-30 21:02:29 +0100

    Deploy to gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { App } from "./App.jsx";
 import { Page2 } from "./Page2";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
