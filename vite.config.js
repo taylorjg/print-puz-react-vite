0a5876de01c0a9bd4204531a5674f74796e21844
Jonathan Taylor <jonathan.taylor@disney.com>
2026-07-20 09:23:35 +0100

    Remove unnecessary stuff from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,19 @@
-/// <reference types="vitest/config" />
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
-import * as path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/print-puz-react-vite/",
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@app", replacement: path.resolve(__dirname, "src") }],
+    alias: [
+      {
+        find: "@app",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
   },
   server: {
-    host: true,
-    port: 5173,
-  },
-  preview: {
-    host: true,
-    port: 5173,
     strictPort: true,
   },
   test: {
